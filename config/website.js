96e14d553ccdf8fcffbd1aaf757f7e4fe4c925d4
hozefam <hozefam@gmail.com>
2019-11-29 16:10:06 +0300

    Initial Commit

---
+++ config/website.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: "Hozefa's Blog", // Navigation and Site Title
+  siteTitleAlt: 'The blog of an MERN stack developer', // Alternative Site title for SEO
+  siteTitleShort: 'blog', // short_name for manifest
+  siteUrl: 'https://hozefam.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: 'This is where they post things about MERN',
+  author: 'Author', // Author for schemaORGJSONLD
+  organization: 'Hozefa',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@hozefam', // Twitter Username
+  ogSiteName: 'Hozefa Blog', // Facebook Site Name
+  ogLanguage: 'en_US',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#5348FF',
+  backgroundColor: '#2b2e3c',
+
+  // Social component
+  twitterUrl: 'https://twitter.com/hozefam/',
+  twitterHandle: '@hozefam',
+  githubUrl: 'https://github.com/hozefam/',
+  githubHandle: 'hozefam',
+  fbAppID: ''
+};
